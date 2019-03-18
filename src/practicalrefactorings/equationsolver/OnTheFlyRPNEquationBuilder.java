988685ad4b73e8bf4615fcadbc3dab85ec5bd230
Jiri Medved <medved@volny.cz>
2019-03-18 06:17:56 +0100

    Extract node

--- src/practicalrefactorings/equationsolver/OnTheFlyRPNEquationBuilder.java
+++ src/practicalrefactorings/equationsolver/OnTheFlyRPNEquationBuilder.java
@@ -10,50 +10,55 @@ import java.util.Stack;
 
 public class OnTheFlyRPNEquationBuilder implements RPNEquationBuilder {
 
-	private Stack<Evaluable> stack = new Stack<>();
+    private Stack<Evaluable> stack = new Stack<>();
 
-	@Override
-	public RPNEquationBuilder push(String token) {
-		try {
-			addNumber(token);
-		} catch (NumberFormatException e) {
-			addNotANumberToken(token);
-		}
-		return this;
-	}
+    @Override
+    public RPNEquationBuilder push(String token) {
+        Evaluable node = null;
+        node = getNode(token);
+        stack.push(node);
+        return this;
+    }
 
-	private void addNotANumberToken(String token) {
-		if (token.length() == 1) {
-			addOperator(token);
-		} else {
-			throw new IllegalArgumentException("Dont understand token: " + token);
-		}
-	}
+    private Evaluable getNode(String token) {
+        Evaluable node;
+        try {
+            node = getNumberFromToken(token);
+        } catch (NumberFormatException e) {
+            node = getNotANumberFromToken(token);
+        }
+        return node;
+    }
 
-	private void addOperator(String token) {
-		try {
-			Evaluable right = stack.pop();
-			Evaluable left = stack.pop();
-			Evaluable operator = new Operator(token.charAt(0),left ,right );
-			stack.push(operator);
-		}
-		catch (EmptyStackException e) {
-			throw new IllegalStateException("Nothing left on the stack for operand");
-		}
-	}
+    private Evaluable getNotANumberFromToken(String token) {
+        if (token.length() == 1) {
+            return getOperatorFromToken(token);
+        } else {
+            throw new IllegalArgumentException("Dont understand token: " + token);
+        }
+    }
 
-	private void addNumber(String token) {
-		int value = Integer.parseInt(token);
-		Evaluable number = new Number(value);
-		stack.push(number);
-	}
+    private Evaluable getOperatorFromToken(String token) {
+        try {
+            Evaluable right = stack.pop();
+            Evaluable left = stack.pop();
+            return new Operator(token.charAt(0), left, right);
+        } catch (EmptyStackException e) {
+            throw new IllegalStateException("Nothing left on the stack for operand");
+        }
+    }
 
-	@Override
-	public Evaluable build() {
-		if (stack.size() != 1) {
-			throw new IllegalStateException("More than one token left on the stack, unbalanced input.");
-		}
-		return stack.pop();
-	}
+    private Evaluable getNumberFromToken(String token) {
+        int value = Integer.parseInt(token);
+        return new Number(value);
+    }
+
+    @Override
+    public Evaluable build() {
+        if (stack.size() != 1) {
+            throw new IllegalStateException("More than one token left on the stack, unbalanced input.");
+        }
+        return stack.pop();
+    }
 
 }
